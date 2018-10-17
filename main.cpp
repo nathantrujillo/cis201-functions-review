b29a3c49273b7f1c201adb676491c2bf45b28a52
Nathan Trujillo <nathantrujillo@cis.vvc.edu>
2018-10-16 19:32:50 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,49 @@
+/*
+File: main.cpp
+Description: Count the a's in a string. 
+Author: Nathan Trujillo
+Email: trujillon@student.vvc.edu
+Date Created: 10/16/18
+*/
+
 #include<iostream>
+#include<string>
+
 
 
 using namespace std;
 
 
+
+
+// The function counts a's in a string
+int count_a_s(string s)
+{
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) 
+    {
+        char character = s.at(i);
+        //cout << i << "s.at(" << i <<") = " << s.at(i) << endl;
+        if (character == 'a' || character == 'A')
+        {
+            count++;
+        }
+    }
+   
+    return count;
+}
+
+
+
 int main()
 {
-    cout << "Hello World"<< endl;
 
-    return 0;
+    string input;
+    cout << "Please type a string: ";
+    getline(cin, input);
+    cout << "There are " << count_a_s(input) << " letter 'a's." << endl;
+    
+
+
+
 }
